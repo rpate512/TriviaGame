c81a0fc96489ff53c2c8bd44da4039ce236191e8
Ryan Pate <ryanpate@ryans-mbp.attlocal.net>
2017-08-17 18:23:44 -0500

    Final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,31 +5,31 @@ $(document).ready(function() {
 	var timer = 30;
 	var intervaId = 30;
 	var values = [];
-	var timer = 3000;
+	var timer = 30;
 	
-	function reset () {
-		gotWrong = 0;
-		gotRight = 0;
-		timer = 3000;
- 		$('input').prop('checked', null); 
-		clearInterval(intervalId);
-		run ();
-	}
+function reset () {
+	gotWrong = 0;
+	gotRight = 0;
+	timer = 30;
+ 	$('input').prop('checked', null); 
+	clearInterval(intervalId);
+	run ();
+}
 
-	function run() {
-		intervalId = setInterval(decrement, 1000);
-		decrement();	
-	}
+function run() {
+	intervalId = setInterval(decrement, 1000);
+	decrement();	
+}
 
-	function decrement(){
-		timer--;
-		$("#timeLeft").html(timer); 
-		
-		if (timer == 0) {
-				alert ("You ran out of time! Try again!");
-				reset ();
-		}	
-	}
+function decrement(){
+	timer--;
+	$("#timeLeft").html(timer); 
+	
+	if (timer == 0) {
+			alert ("You ran out of time! Try again!");
+			reset ();
+	}	
+}
 
 	run();
 
@@ -49,13 +49,21 @@ function checker() {
 		}
 	}
 }
+
+function gameOver () {
+	clearInterval(timer);
+}
+
 $("#submit").click(function () {
 	checker();
-	reset();
 	$("#content").hide();
-	$(".container").append("Correct: " + gotRight);
-	$(".container").append("Incorrect: " + gotwrong);
-})
+	$(".container").append('#gameOver').html('<h2> Game Over!</h2><span> Correct: ' + gotRight + ' Incorrect: ' + gotWrong);
+	$("<br>")
+	$(".container").append("<button id='newGame'>" + "Play Again" + "</button>");
+
+	});
+
+
 });
 
 
