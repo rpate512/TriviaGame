1ce865ad50527f62cc3f37513b0637d3aa263674
Ryan Pate <ryanpate@ryans-mbp.attlocal.net>
2017-08-17 17:37:43 -0500

    updated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,61 @@
 $(document).ready(function() {
 
-var gotRight = 0;
-var gotWrong = 0;
-var timer = 30;
-var intervaId = 30;
-var values = [];
+	var gotRight = 0;
+	var gotWrong = 0;
+	var timer = 30;
+	var intervaId = 30;
+	var values = [];
+	var timer = 3000;
+	
+	function reset () {
+		gotWrong = 0;
+		gotRight = 0;
+		timer = 3000;
+ 		$('input').prop('checked', null); 
+		clearInterval(intervalId);
+		run ();
+	}
 
-function run(timer) {
-	intervalId = setInterval(decrement, 1000);
-	decrement();	
-}
+	function run() {
+		intervalId = setInterval(decrement, 1000);
+		decrement();	
+	}
 
-function reset () {
-	gotWrong = 0;
-	gotRight = 0;
-	timer = 30;
-	clearInterval(intervalId)
-}
-
-run();
-
-function run(timer) {
-	intervalId = setInterval(decrement, 1000);
-	decrement();	
-}
-function decrement(timer){
-		timer --;
-		$("timeLeft").html("Time Remaining: " + timer); 
+	function decrement(){
+		timer--;
+		$("#timeLeft").html(timer); 
 		
 		if (timer == 0) {
-			function outOfTimeLoss () {
 				alert ("You ran out of time! Try again!");
 				reset ();
 		}	
 	}
 
-function logValue (values) {
-	playerChoice.attr("value");
-	appendTo(values);
+	run();
 
-var playerChoice = $("#button").on("click")
-	logValue ();
 
+function checker() {
+	var valueAmount;
+	var answers= $("input:checked");
+
+	for (var i = 0; i < answers.length; i++) {
+		answers[i]
+		valueAmount = answers.eq(i).val().trim();
+
+		if (valueAmount == 1) {
+			gotRight++;
+		} else {
+			gotWrong++;
 		}
 	}
+}
+$("#submit").click(function () {
+	checker();
+	reset();
+	$("#content").hide();
+	$(".container").append("Correct: " + gotRight);
+	$(".container").append("Incorrect: " + gotwrong);
+})
 });
+
+
